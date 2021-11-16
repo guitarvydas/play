d2530fa3db0f7e2f3ebd2367e8ad7da715feee2d
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-16 10:49:54 -0500

    incantations to run hello.js

--- docker2/run.bash
+++ docker2/run.bash
@@ -1,3 +1,15 @@
 #!/bin/bash
+# docker build -t simple .
+# testdir=`realpath ../tests`
+# docker run -v "${testdir}/:/app" simple test1.txt
+
+# docker build -t simple .
+# fileInMyEnvironment=`realdir $1`
+# docker run -v "${testdir}/:/app" simple test1.txt
+
 docker build -t simple .
-docker run simple ../tests/test1.txt
+file1=../tests/test1.txt
+fullname=`realpath ${file1}`
+dir=`dirname ${fullname}`
+fname=`basename ${file1}`
+docker run -v "${dir}/:/app" simple /app/${fname}
--- docker2/tasks.bash
+++ docker2/tasks.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-./task.bash ../tests/test1.txt ../tests/test2.txt
-./atask.bash ../tests/test1.txt ../tests/test2.txt
+./task.bash $1
+./atask.bash $1
