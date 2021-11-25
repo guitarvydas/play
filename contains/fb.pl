0af49517ef59af50421206ed25864f6efd1ae4bc
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-25 11:37:07 -0500

    wip indirect contains

--- contains/fb.pl
+++ contains/fb.pl
@@ -3,8 +3,22 @@ contains(r1,r3).
 contains(r1,r4).
 contains(r2,r3).
 contains(r2,r4).
-contains(r4,r4).
+contains(r3,r4).
 
 transitive_contains(X,Y):-
     contains(X,Y),
     X \= Y.
+
+transitive_contains(X,Z):-
+    contains(X,Y),
+    \+ contains(X,Z),
+    transitive_contains(Y,Z).
+
+indirect_contains(X,Z):-
+    contains(X,Y),
+    \+ contains(X,Z),
+    indirect_contains(Y,Z).
+indirect_contains(X,Z):-
+    contains(X,Y),
+    \+ contains(X,Z),
+    contains(Y,Z).
