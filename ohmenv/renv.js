29ea85977363bf852fa62bd0cf40ee729c230dc6
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-19 08:11:18 -0400

    27

--- ohmenv/renv.js
+++ ohmenv/renv.js
@@ -21,22 +21,20 @@ function patternMatch (grammar, phrase) {
     }
 }
 
-var depth = 0;
-
-const opRules = {
-    Main : function (_cs) { depth = 0; _cs.op (); },
-    Chars_recursive: function (_c, _cs) { depth += 1; console.log (`char=${_c.op ()} depth=${depth}`); _cs.op ();},
-    Chars_bottom: function (_c) { depth += 1; console.log (`char=${_c.op ()} depth=${depth}`); },
-    char: function (_c) { return _c.op (); },
+const opActions = {
+    Main : function (_cs) { _cs.op (27); },
+    Chars_recursive: function (_c, _cs) { var depth = this.args.depth; console.log (`char=${_c.op (depth)} depth=${depth}`); _cs.op (depth + 1);},
+    Chars_bottom: function (_c) { var depth = this.args.depth; console.log (`char=${_c.op (depth)} depth=${depth}`); },
+    char: function (_c) { var depth = this.args.depth; return _c.op (depth); },
     _terminal: function () { return this.sourceString; },
-    _iter: function (...children) { return children.map(c => c.op ()); }
+    _iter: function (...children) { var depth = this.args.depth; return children.map(c => c.op (depth)); }
 };
 
-function do_op (cst, walker_envelope, rules) {
+function do_op (cst, walker_skeleton, actions) {
     if (cst.succeeded ()) {
-	walker_envelope.addOperation ('op', rules);
-        let treeWalker = walker_envelope (cst);
-        let result = treeWalker.op ();
+	walker_skeleton.addOperation ('op(depth)', actions);
+        let treeWalker = walker_skeleton (cst);
+        let result = treeWalker.op (null);
 	return result;
     } else {
 	return "Parse FAILED";
@@ -48,6 +46,6 @@ const inphrase = String.raw`
 abc
 `;
 
-var [result, walker_envelope] = patternMatch (Ohm.grammar (grammar), inphrase);
+var [result, walker_skeleton] = patternMatch (Ohm.grammar (grammar), inphrase);
 var cst = result;
-console.log (do_op (cst, walker_envelope, opRules));
+console.log (do_op (cst, walker_skeleton, opActions));
--- ohmenv/renv2.js
+++ ohmenv/renv2.js
@@ -22,7 +22,7 @@ function patternMatch (grammar, phrase) {
 }
 
 const opActions = {
-    Main : function (_cs) { _cs.op (0); },
+    Main : function (_cs) { _cs.op (27); },
     Chars_recursive: function (_c, _cs) { var depth = this.args.depth; console.log (`char=${_c.op (depth)} depth=${depth}`); _cs.op (depth + 1);},
     Chars_bottom: function (_c) { var depth = this.args.depth; console.log (`char=${_c.op (depth)} depth=${depth}`); },
     char: function (_c) { var depth = this.args.depth; return _c.op (depth); },
@@ -34,7 +34,7 @@ function do_op (cst, walker_skeleton, actions) {
     if (cst.succeeded ()) {
 	walker_skeleton.addOperation ('op(depth)', actions);
         let treeWalker = walker_skeleton (cst);
-        let result = treeWalker.op (0);
+        let result = treeWalker.op (8);
 	return result;
     } else {
 	return "Parse FAILED";
