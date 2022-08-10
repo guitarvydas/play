4d0bee5ba7959cf360c7362923fc0a345676cd0c
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-10 07:51:31 -0400

    markdown .ohm

---
+++ drawio/run.bash
@@ -0,0 +1,1 @@
+./unc.js test.drawio
--- drawio/unc.js
+++ drawio/unc.js
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 //'use strict'
 
-const fs = require ('fs');
+var atob = require ('atob'); // npm install atob
+var pako = require ('pako'); // npm install pako
+var fs   = require ('fs');
+
 var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+console.log (argv._[0]);
+var encoded = fs.readFileSync (argv._[0], 'utf-8');
+console.log (encoded);
+var decoded = inline_decodeMxDiagram (encoded);
+console.log (decoded);
+
+function inline_decodeMxDiagram (encoded) {
+    var data = atob (encoded);
+    console.log (1);
+    console.log (data);    
+    var inf = pako.inflateRaw (
+	Uint8Array.from (data, c=>c.charCodeAt (0)), {to: 'string'})
+    console.log (2);
+    console.log (inf);    
+    var str = decodeURIComponent (inf);
+    return str;
+}
+
