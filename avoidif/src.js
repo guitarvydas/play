36702ba631ebaef93ad2b0f024d05792f1ada4d7
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-02 00:45:25 -0400

    avoidif subrepos

---
+++ avoidif/src.js
@@ -0,0 +1,14 @@
+const src = String.raw`
+{
+  sender: {"cell_6", "a"}
+  receiver: {"cell_7", "b"}
+}
+{
+  sender: {"cell_7", "c"}
+  receiver: {"cell_8", "d"}
+}
+{
+  sender: {"cell_8", "e"}
+  receiver: {"cell_6", "f"}
+}
+`;
