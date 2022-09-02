a022a9b42b04da3c47266dce9ea6e1945ffb61ea
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-02 07:37:12 -0400

    Self Sender/Receiver

--- avoidif/connections3.js
+++ avoidif/connections3.js
@@ -1,13 +1,18 @@
 const connections3grammar = String.raw`
 Connections {
   Connections = Connection+
-  Connection = "{" Sender Receiver "}"
+  Connection = 
+    | "{" SelfSender SelfReceiver "}" -- passThrough
+    | "{" SelfSender Receiver "}" -- down
+    | "{" Sender SelfReceiver "}" -- up
+    | "{" Sender Receiver "}" -- route
+  SelfSender = "sender" ":" "{" SelfComponentName "," PortName "}"
   Sender = "sender" ":" "{" ComponentName "," PortName "}"
+  SelfReceiver = "receiver" ":" "{" SelfComponentName "," PortName "}"
   Receiver = "receiver" ":" "{" ComponentName "," PortName "}"
 
-  ComponentName = SelfComponentName | NonSelfComponentName
-    SelfComponentName = "self"
-    NonSelfComponentName = string
+  SelfComponentName = "self"
+  ComponentName = string
   PortName = string
 
   string = dq (~dq any)* dq
@@ -18,10 +23,16 @@ Connections {
 const connections3fmt = String.raw`
 Connections {
   Connections [Connections+] = ‛⟨Connections⟩’
-  Connection [lb Sender Receiver rb] = ‛\n⟨lb⟩⟨Sender⟩⟨Receiver⟩⟨rb⟩’
+  Connection_route [lb Sender Receiver rb] = ‛\n⟨lb⟩⟨Sender⟩⟨Receiver⟩⟨rb⟩’
+  Connection_down [lb Sender Receiver rb] = ‛\n⟨lb⟩⟨Sender⟩⟨Receiver⟩⟨rb⟩’
+  Connection_up [lb Sender Receiver rb] = ‛\n⟨lb⟩⟨Sender⟩⟨Receiver⟩⟨rb⟩’
+  Connection_passThrough [lb Sender Receiver rb] = ‛\n⟨lb⟩⟨Sender⟩⟨Receiver⟩⟨rb⟩’
   Sender [ksender kcolon lb ComponentName kcomma PortName rb] = ‛⟨ksender⟩⟨kcolon⟩⟨lb⟩⟨ComponentName⟩⟨kcomma⟩⟨PortName⟩⟨rb⟩’
+  SelfSender [ksender kcolon lb ComponentName kcomma PortName rb] = ‛⟨ksender⟩⟨kcolon⟩⟨lb⟩⟨ComponentName⟩⟨kcomma⟩⟨PortName⟩⟨rb⟩’
   Receiver [kreceiver kcolon lb ComponentName kcomma PortName rb] = ‛⟨kreceiver⟩⟨kcolon⟩⟨lb⟩⟨ComponentName⟩⟨kcomma⟩⟨PortName⟩⟨rb⟩’
+  SelfReceiver [kreceiver kcolon lb ComponentName kcomma PortName rb] = ‛⟨kreceiver⟩⟨kcolon⟩⟨lb⟩⟨ComponentName⟩⟨kcomma⟩⟨PortName⟩⟨rb⟩’
 
+  SelfComponentName [s] = ‛self’
   ComponentName [s] = ‛⟨s⟩’
   PortName [s] = ‛⟨s⟩’
 
