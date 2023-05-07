6ecbe91ca9983ab1baeb034f9a7be7670b3af183
Paul Tarvydas <guitarvydas@gmail.com>
2023-05-07 08:58:10 -0400

    full md formatting on test case

--- kinopio/k2md.bash
+++ kinopio/k2md.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -H "Authorization: $2" https://api.kinopio.club/space/$1 > kinopio.tmp
+curl -s -H "Authorization: $2" https://api.kinopio.club/space/$1 > kinopio.tmp
 
 echo ---
 echo -n "title: "
@@ -10,6 +10,6 @@ cat kinopio.tmp | jq .updatedAt
 echo "layout: layouts/post.njk"
 echo ---
 
-# cat kinopio.tmp | jq -r '.cards | sort_by(.y) | sort_by(.x) | .[] | .name + "\n"'
+cat kinopio.tmp | jq -r '.cards | sort_by(.y) | sort_by(.x) | .[] | .name + "\n"'
 
 # rm kinopio.tmp
