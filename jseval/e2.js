720176959b30fe5f042807e62163d5a69a306228
Paul Tarvydas <paul.tarvydas@gmail.com>
2023-09-04 16:16:58 -0400

    abandon playing with eval()

--- jseval/e2.js
+++ jseval/e2.js
@@ -1,11 +1,18 @@
-const fctStr1 = "function a() {return 'in a';} function b() {return 'in b'}";
+const fctStr1 = `
+var tracing = 42;
+function a() {return 'in a';}
+function b() {return this;}
+`;
+
 const fct1 = eval ?. (fctStr1);
 console.log (fct1);
 
-const nsStr = "{x : 1, y : 2, f: function () { return `${a ()} and in f` }}";
+const nsStr = "{x : 1, y : 2, z : a, f: function () { return `${a ()} and in f and tracing=${tracing}` }}";
 const ns = eval ?. ('(' + nsStr + ')');
 
 console.log (a());
 console.log (b());
+console.log (global);
+console.log (f_locals);
 console.log (ns);
 console.log (ns.f());
