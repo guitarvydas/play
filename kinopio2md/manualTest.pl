e7639b61ef3986346aeffdadd3745167c33b8cda
Paul Tarvydas <paul.tarvydas@gmail.com>
2023-10-28 05:09:15 -0400

    back to mnemonic nodes

--- kinopio2md/manualTest.pl
+++ kinopio2md/manualTest.pl
@@ -1,19 +1,20 @@
 :- use_module(library(ugraphs)).
 
-text(nT, 'control flow is not data').
-text(nU, 'control flow is _interpretation_ of data - dynamic flow').
-text(nV, 'a CPU is an interpreter').
-text(nW, 'a CPU interprets _opcodes_').
-text(nX, 'data is _static_ - and is not interpreted by the underlying system').
-text(nY, 'static data is interpreted by the _program_, not the CPU').
-text(nZ, 'a CPU interprets _opcodes_ so quickly that we think of it as something other than interpretation').
-%% text(nT, 'T').
-%% text(nU, 'U').
-%% text(nV, 'V').
-%% text(nW, 'W').
-%% text(nX, 'X').
-%% text(nY, 'Y').
-%% text(nZ, 'Z').
+%% text(nT, 'control flow is not data').
+%% text(nU, 'control flow is _interpretation_ of data - dynamic flow').
+%% text(nV, 'a CPU is an interpreter').
+%% text(nW, 'a CPU interprets _opcodes_').
+%% text(nX, 'data is _static_ - and is not interpreted by the underlying system').
+%% text(nY, 'static data is interpreted by the _program_, not the CPU').
+%% text(nZ, 'a CPU interprets _opcodes_ so quickly that we think of it as something other than interpretation').
+
+text(nT, 'T').
+text(nU, 'U').
+text(nV, 'V').
+text(nW, 'W').
+text(nX, 'X').
+text(nY, 'Y').
+text(nZ, 'Z').
 
 parent(nU, nT).
 parent(nX, nT).
